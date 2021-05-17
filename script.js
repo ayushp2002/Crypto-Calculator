4de059558d65e3da2ac0a597aa606f1cf98049c8
Ayush Priyadarshi <31592771+ayushp2002@users.noreply.github.com>
2021-05-17 15:57:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '992723977865-h83ciu7b4e9csj9hr6tdq652fo9d42em.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBsN2AIqojIArdGkEL1JJ3S2Zai6P9SbKo';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+// var SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+// HTML Elements declaration
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+// ================================================
+// Begin of utility functions
+// ================================================
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+// ================================================
+// Begin of running functions
+// ================================================
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  }, function(error) {
+    appendPre(JSON.stringify(error, null, 2));
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'inline';
+    listSpreadsheetData();
+  } else {
+    authorizeButton.style.display = 'inline';
+    signoutButton.style.display = 'none';
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Print the data in the spreadsheet:
+ * https://drive.google.com/file/d/1shMQ8qPw4NN_xkFLpyymitzfYrbGxtRl/view?usp=sharing
+ */
+// TODO: fix the error:
+// {error: {code: 400, message: "This operation is not supported for this document",…}}
+//  error: {code: 400, message: "This operation is not supported for this document",…}
+//    code: 400
+//    message: "This operation is not supported for this document"
+//    status: "FAILED_PRECONDITION"
+function listSpreadsheetData() {
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1shMQ8qPw4NN_xkFLpyymitzfYrbGxtRl',
+    range: 'INR History!A2:H',
+  }).then(function(response) {
+    var range = response.result;
+    if (range.values.length > 0) {
+      appendPre('Currency, Quantity, Price:');
+      for (i = 0; i < range.values.length; i++) {
+        var row = range.values[i];
+        // Print columns A and E, which correspond to indices 0 and 4.
+        appendPre(row[1] + ', ' + row[2] + ', ' + row[3]);
+      }
+    } else {
+      appendPre('No data found.');
+    }
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+}
